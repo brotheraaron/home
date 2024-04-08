4c6856a6ac81f5f253eaf9044caa6e593e00251c
Aaron Berger <aaronberger.webmail@gmail.com>
2024-04-07 22:55:26 -0700

    Code from flask and sqlite tutorial.

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
-from flask import Flask, render_template, request, url_for, flash, redirect
+import sqlite3
+from flask import Flask, render_template, request, url_for, flash, redirect, abort
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '168498626ac062927cd765f05c97ac5d99080d48d83f50a9'
 
-messages = [{'title': 'Message One',
-             'content': 'Message One Content'},
-            {'title': 'Message Two',
-             'content': 'Message Two Content'}
-            ]
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',(post_id,)).fetchone
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', messages=messages)
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
 
 
 @app.route('/create/', methods=('GET', 'POST'))
@@ -25,11 +37,52 @@ def create():
         elif not content:
             flash('Content is required!')
         else:
-            messages.append({'title': title, 'content': content})
-            return redirect(url_for('index'))
+          conn = get_db_connection()
+          conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)', (title, content))
+          conn.commit()
+          conn.close()
+          return redirect(url_for('index'))
 
     return render_template('create.html')
 
 @app.route('/about/')
 def about():
     return render_template('about.html')
+
+
+# ...
+
+@app.route('/<int:id>/edit/', methods=('GET', 'POST'))
+def edit(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+
+        elif not content:
+            flash('Content is required!')
+
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ?' ' WHERE id = ?',(title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('edit.html', post=post)
+
+# ...
+
+@app.route('/<int:id>/delete/', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    return redirect(url_for('index'))
+
